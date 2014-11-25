b0ef271e5afd858a964e46fa90cabe58e4022710
lvyinrun <lvyinrun@gmail.com>
2014-11-25 16:52:21 +0800

    v0.0.1
    html dragged images.
    url patterns finished

--- myRef/views.py
+++ myRef/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext,loader
 
+from myRef.models import RefFile
 # Create your views here.
+def index(request):
+    latest_list = RefFile.objects.all()
+    template = loader.get_template('myRef/abc.html')
+    context = RequestContext(request,{'latest_list':latest_list,})
+    return HttpResponse(template.render(context))
+
